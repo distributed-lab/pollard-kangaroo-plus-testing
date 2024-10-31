90f72be466c3c7e2e979187c09787e2a577e59f0
Jonikorjk <zenaserdykov2004@gmail.com>
2024-10-31 18:12:11 +0200

    Initial Commit

---
+++ kangaroo-ios-clientTests/kangaroo_ios_clientTests.swift
@@ -0,0 +1,17 @@
+//
+//  kangaroo_ios_clientTests.swift
+//  kangaroo-ios-clientTests
+//
+//  Created by Yevhenii Serdiukov on 31.10.2024.
+//
+
+import Testing
+@testable import kangaroo_ios_client
+
+struct kangaroo_ios_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
