8e04cd012431d56d1e8185a3e48907853247a732
dovgopoly <idovgopoly@gmail.com>
2024-12-24 18:42:08 +0200

    fix

--- pollard-kangaroo/src/wasm.rs
+++ pollard-kangaroo/src/wasm.rs
@@ -9,27 +9,13 @@ pub struct WASMKangaroo(Kangaroo);
 
 #[wasm_bindgen]
 pub fn create_kangaroo(
-    s: Vec<js_sys::Uint8Array>,
-    slog: Vec<js_sys::Uint8Array>,
-    table_point: Vec<js_sys::Uint8Array>,
-    table_value: Vec<js_sys::Uint8Array>,
+    table_object: JsValue,
     n: u64,
     w: u64,
     r: u64,
     bits: u8,
 ) -> Result<WASMKangaroo, JsError> {
-    let table_params = TableParams {
-        s: s.into_iter().map(|x| hex::encode(x.to_vec())).collect(),
-        slog: slog.into_iter().map(|x| hex::encode(x.to_vec())).collect(),
-        table: table_point
-            .into_iter()
-            .zip(table_value.into_iter())
-            .map(|(point, value)| TableEntry {
-                point: hex::encode(point.to_vec()),
-                value: hex::encode(value.to_vec()),
-            })
-            .collect(),
-    };
+    let table_params: TableParams = serde_wasm_bindgen::from_value(table_object)?;
     let table = Table::new(table_params);
 
     Ok(WASMKangaroo(Kangaroo::new(n, w, r, bits, table)))
